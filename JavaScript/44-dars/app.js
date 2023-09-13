b88dd0c9e54bf8c7f5be240f08cc944e8b9ea69f
Sattorov Kamronbek <pubggameruzb.sila@gmail.com>
2023-09-13 15:31:11 +0500

    START

--- JavaScript/44-dars/app.js
+++ JavaScript/44-dars/app.js
@@ -1,8 +1,23 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Sattorov')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Kamronbek')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('<strong>Navruzovich</strong>')
+    .pauseFor(2500)
+    .start();
 // 44-dars
 
-<layout>
+{/* <layout>
     Webix UI kutubxonasidan JavaScript Layout vidjeti boshqa komponentlar sahifaga qanday joylashtirilishini belgilaydi. Layout-dan foydalanib, siz sahifani barcha boshqa komponentlar joylashtirilgan qatorlar va ustunlarga bo'lishingiz mumkin. Foydalanuvchilarga ilovangizning ma'lum bir sohasining o'lchamini o'zgartirishga imkon berish uchun komponentlar o'rtasida sudrab olinadigan chegara yaratishingiz mumkin.
-</layout>
+</layout> */}
 
 
 
