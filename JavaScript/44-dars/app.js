d489f05a591bcc4a2251c7513a11bcc51663eccb
Sattorov Kamronbek <pubggameruzb.sila@gmail.com>
2023-09-13 15:09:18 +0500

    2start

--- JavaScript/44-dars/app.js
+++ JavaScript/44-dars/app.js
@@ -1,5 +1,9 @@
 // 44-dars
 
+<layout>
+    Webix UI kutubxonasidan JavaScript Layout vidjeti boshqa komponentlar sahifaga qanday joylashtirilishini belgilaydi. Layout-dan foydalanib, siz sahifani barcha boshqa komponentlar joylashtirilgan qatorlar va ustunlarga bo'lishingiz mumkin. Foydalanuvchilarga ilovangizning ma'lum bir sohasining o'lchamini o'zgartirishga imkon berish uchun komponentlar o'rtasida sudrab olinadigan chegara yaratishingiz mumkin.
+</layout>
+
 // ========================================================
 
 // TASKS
