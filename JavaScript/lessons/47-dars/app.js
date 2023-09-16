01379c5dd051d03fad3ff27f2832e4b525da98ea
Sattorov Kamronbek <pubggameruzb.sila@gmail.com>
2023-09-16 17:24:00 +0500

    dsfds

--- JavaScript/lessons/47-dars/app.js
+++ JavaScript/lessons/47-dars/app.js
@@ -20,6 +20,7 @@ const Person = function(firstName, lastName, age){
 }
 // 1.Bo'sh {} yaratiladi
 // 2.this yangi ochilgan bo'sh {} ga o'zlashadi va Function ishlaydi.
+// 3. Objectimiz protorype bog'lanadi.
 
 
 
