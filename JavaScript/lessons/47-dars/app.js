2055cbc5a21e4e50f6e0a30ab1107b0b6a4dfbfb
Sattorov Kamronbek <pubggameruzb.sila@gmail.com>
2023-09-16 17:19:20 +0500

    sds

--- JavaScript/lessons/47-dars/app.js
+++ JavaScript/lessons/47-dars/app.js
@@ -1,22 +1,11 @@
 // 47-dars start
 
 // Object literal
-const person = {
-    firstName: 'Kamronbek',
-    lastName: 'Sattorov',
-    age: 50,
-}
+// const person = {
+//     firstName: 'Kamronbek',
+//     lastName: 'Sattorov',
+//     age: 50,
+// }
 
-const person2 = {
-    firstName: 'ss',
-    lastName: 'dd',
-    age: 56,
-}
-
-const person3 = {
-    firstName: 'fff',
-    lastName: 'hhh',
-    age: 12,
-}
 
 // 47-dars end
